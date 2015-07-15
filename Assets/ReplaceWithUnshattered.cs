0c46353803da5b6c166a0af2cd3b0e3af606c6bf
Shahan Akhter <shahan3.14@gmail.com>
2015-07-15 12:26:10 +0300

    set up basic project

---
+++ Assets/ReplaceWithUnshattered.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class ReplaceWithUnshattered : MonoBehaviour
+{
+
+    public GameObject pointyHands;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+
+}
